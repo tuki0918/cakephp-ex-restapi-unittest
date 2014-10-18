3d8dc434fdbf092ccd7e2cc8cd7b1ba0d1ec896e
Yuta Yamamoto <tuki0918@gmail.com>
2014-10-18 13:52:17 +0900

    refactor: ApiController

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -2,21 +2,37 @@
 App::uses('AppController', 'Controller');
 
 class ApiController extends AppController {
+
     public $uses = array('Post');
-    // RequestHandlerコンポーネントを使用
     public $components = array('RequestHandler');
 
+    public $autoLayout = false;
+
+    private $_result = null;
+
     public function beforeRender() {
+        parent::beforeRender();
+
+        $this->viewJson();
+    }
+
+    public function setResult($data) {
+        $this->_result = $data;
+    }
+
+    private function viewJson() {
+        if (!empty($this->_result)) {
+            $data = $this->_result;
+            $this->viewClass = 'Json';
+            $this->set(compact('data'));
+            $this->set('_serialize', 'data');            
+        }
     }
 
     public function index() {
-        $json = array(
+        $data = array(
             1,2,4,5
         );
-
-
-        $this->viewClass = 'Json';
-        $this->set(compact('json'));
-        $this->set('_serialize', 'json');
+        $this->setResult($data);
     }
 }
