aa1dc8e888c01f005e5fd0c0c852c8ebe11d7f96
Yuta Yamamoto <tuki0918@gmail.com>
2014-10-18 14:59:50 +0900

    refactor: ApiControllerTest
    browser: http://localhost/resttest/app/webroot/test.php
    command: C:\xampp\htdocs\resttest\app>cake testsuite app Controller/ApiController --stderr

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -13,25 +13,48 @@ class ApiController extends AppController {
     public function beforeRender() {
         parent::beforeRender();
 
-        $this->viewJson();
+        $this->_viewJson();
     }
 
     public function setResult($data) {
         $this->_result = $data;
     }
 
-    private function viewJson() {
+    private function _viewJson() {
         if (!empty($this->_result)) {
             $data = $this->_result;
             $this->viewClass = 'Json';
             $this->set(compact('data'));
-            $this->set('_serialize', 'data');            
+            $this->set('_serialize', 'data');
         }
     }
 
     public function index() {
         $data = array(
             1,2,4,5
+        );
+        $this->setResult($data);
+    }
+
+    public function items() {
+        $data = array(
+            'data' => array(
+                array(
+                    'id'    => 1,
+                    'name'  => 'item01',
+                    'price' => 111,
+                ),
+                array(
+                    'id'    => 2,
+                    'name'  => 'item02',
+                    'price' => 222,
+                ),
+                array(
+                    'id'    => 3,
+                    'name'  => 'item03',
+                    'price' => 333,
+                ),
+            )
         );
         $this->setResult($data);
     }
--- app/Test/Case/Controller/ApiControllerTest.php
+++ app/Test/Case/Controller/ApiControllerTest.php
@@ -17,13 +17,51 @@ class ApiControllerTest extends ControllerTestCase {
 	// );
 
     public function testIndex() {
-        $result = $this->testAction('/api');
-        $json = $this->vars['json'];
+        $data = array();
+        $result = $this->testAction('/api',
+            array(
+                'data'   => $data,
+                'method' => 'get',
+                'return' => 'contents',
+            )
+        );
+        $result = json_decode($result, true);
         $expected = array(
             1,2,4,5
         );
-        $this->assertEquals($expected, $json);
-		debug($json);
+        $this->assertEquals($expected, $result);
     }
 
+
+    public function testItems() {
+        $data = array();
+        $result = $this->testAction('/api/items',
+            array(
+                'data'   => $data,
+                'method' => 'get',
+                'return' => 'contents',
+            )
+        );
+        $result = json_decode($result, true);
+        $expected = array(
+            'data' => array(
+                array(
+                    'id'    => 1,
+                    'name'  => 'item01',
+                    'price' => 111,
+                ),
+                array(
+                    'id'    => 2,
+                    'name'  => 'item02',
+                    'price' => 222,
+                ),
+                array(
+                    'id'    => 3,
+                    'name'  => 'item03',
+                    'price' => 333,
+                ),
+            )
+        );
+        $this->assertEquals($expected, $result);
+    }
 }
