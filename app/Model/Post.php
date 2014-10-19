95e7115c2e3b3ba4b6323ac7d02f3343b33664cd
Yuta Yamamoto <tuki0918@gmail.com>
2014-10-19 11:10:13 +0900

    delete post model validation

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -68,15 +68,5 @@ class Post extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'title' => array(
-			'maxLength' => array(
-				'rule' => array('maxLength'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 	);
 }
