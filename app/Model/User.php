543f04da9029d1f7cde72826a4dc7903f4a79b7e
Yuta Yamamoto <tuki0918@gmail.com>
2014-10-29 00:32:58 +0900

    Fix: password changed hash

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,5 +1,7 @@
 <?php
 App::uses('AppModel', 'Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
 /**
  * User Model
  *
@@ -74,5 +76,13 @@ class User extends AppModel {
 			'counterQuery' => ''
 		)
 	);
+
+	public function beforeSave($options = array()) {
+	    if (isset($this->data[$this->alias]['password'])) {
+	        $passwordHasher = new SimplePasswordHasher();
+	        $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
+	    }
+	    return true;
+	}
 
 }
