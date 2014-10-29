21fd5087c4c89c559ff7260271e97370bba27692
Yuta Yamamoto <tuki0918@gmail.com>
2014-10-30 00:26:28 +0900

    Fix: post fixture, add user_id field

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,6 +12,7 @@ class PostFixture extends CakeTestFixture {
  */
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true),
 		'status' => array('type' => 'integer', 'null' => false, 'default' => 0, 'unsigned' => false),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
@@ -31,6 +32,7 @@ class PostFixture extends CakeTestFixture {
 	public $records = array(
 		array(
 			'id'       => 1,
+			'user_id'  => 1,
 			'status'   => 0,
 			'title'    => 'title1',
 			'body'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -39,6 +41,7 @@ class PostFixture extends CakeTestFixture {
 		),
 		array(
 			'id'       => 2,
+			'user_id'  => 1,
 			'status'   => 1,
 			'title'    => 'title2',
 			'body'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -47,6 +50,7 @@ class PostFixture extends CakeTestFixture {
 		),
 		array(
 			'id'       => 3,
+			'user_id'  => 1,
 			'status'   => 1,
 			'title'    => 'title3',
 			'body'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -55,6 +59,7 @@ class PostFixture extends CakeTestFixture {
 		),
 		array(
 			'id'       => 4,
+			'user_id'  => 2,
 			'status'   => 0,
 			'title'    => 'title4',
 			'body'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -63,6 +68,7 @@ class PostFixture extends CakeTestFixture {
 		),
 		array(
 			'id'       => 5,
+			'user_id'  => 3,
 			'status'   => 0,
 			'title'    => 'title5',
 			'body'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
